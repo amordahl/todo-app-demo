42663f15dad19bd5bb6550a3bfadb2dc3c9fa90d
Austin Mordahl <amordahl@uic.edu>
2025-09-24 09:53:04 -0500

    backtrack

--- todoapp/src/main/scala/edu/uic/Main.scala
+++ todoapp/src/main/scala/edu/uic/Main.scala
@@ -13,16 +13,10 @@ import scala.concurrent.duration.DurationInt
 object Main extends MainRoutes:
   override def host: String = "0.0.0.0"
 
-  // Redis client information
-  given ActorSystem = ActorSystem()
-  val redisHost     = sys.env.getOrElse("REDIS_HOST", "localhost")
-  val redis         = RedisClient(redisHost, 6379)
-
   // Postgres db info
-  val dbUrl =
-    sys.env.getOrElse("DATABASE_URL", "jdbc:postgresql://localhost:5432/todos")
-  val dbUser = sys.env.getOrElse("DATABASE_USER", "user")
-  val dbPass = sys.env.getOrElse("DATABSE_PASSWORD", "password")
+  val dbUrl = "jdbc:postgresql://localhost:5432/todos"
+  val dbUser = "postgres"
+  val dbPass = "postgres"
   val db = Database.forURL(
     dbUrl,
     user = dbUser,
@@ -34,27 +28,16 @@ object Main extends MainRoutes:
 
   @cask.get("/todos")
   def getTodos() =
-    val cachedFut = redis.get[String]("todos")
-    val resultFut = cachedFut.flatMap:
-      // Already have the cached result
-      case Some(cached) =>
-        Future.successful(cask.Response(
-          cached,
-          headers = Seq("Content-Type" -> "application/json")
-        ))
-      // No cached result
-      case None =>
-        val query = todos.result
-        db.run(query).flatMap: result =>
-          val json = result.map(t =>
-            Obj("id" -> t.id, "task" -> t.task, "done" -> t.done)
-          ).toSeq
-          val serialized = ujson.write(json)
-          redis.set("todos", serialized).map: _ =>
-            cask.Response(
-              serialized,
-              headers = Seq("Content-Type" -> "application/json")
-            )
+    val query = todos.result
+    val resultFut = db.run(query).flatMap: result =>
+      val json = result.map(t =>
+        Obj("id" -> t.id, "task" -> t.task, "done" -> t.done)
+      ).toSeq
+      val serialized = ujson.write(json)
+      Future.successful(cask.Response(
+        serialized,
+        headers = Seq("Content-Type" -> "application/json")
+      ))
     Await.result(resultFut, 5.seconds)
   end getTodos
 
@@ -65,10 +48,7 @@ object Main extends MainRoutes:
       task.str,
       done = false
     )
-    val insertFut = db.run(insert).flatMap: newId =>
-      redis.del("todos").map: _ =>
-        Obj("id" -> newId, "task" -> task.str, "done" -> false)
-    Await.result(insertFut, 5.seconds)
+    Await.result(db.run(insert), 5.seconds)
   end addTodo
 
   initialize()
